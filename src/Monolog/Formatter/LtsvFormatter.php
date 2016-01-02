7235555cef5af5774f63d27928a04a3513935bbd
Yuki Fujii <fjyuuk@gmail.com>
2016-01-02 12:11:20 +0900

    add LtsvFormatter

---
+++ src/Monolog/Formatter/LtsvFormatter.php
@@ -0,0 +1,108 @@
+<?php
+namespace Hikaeme\Monolog\Formatter;
+
+use Hikaeme\Text\Ltsv\LtsvLineBuilder;
+use Monolog\Formatter\NormalizerFormatter;
+
+/**
+ * Formats incoming records into a line of LTSV.
+ */
+class LtsvFormatter extends NormalizerFormatter
+{
+    /** @var array */
+    protected $labeling;
+
+    /** @var bool */
+    protected $includeContext;
+
+    /** @var bool */
+    protected $includeExtra;
+
+    /** @var array */
+    protected $labelReplacement;
+
+    /** @var array */
+    protected $valueReplacement;
+
+    /**
+     * @param string $dateFormat The format of the timestamp: one supported by DateTime::format.
+     * @param array $labeling Associative array of a Monolog record to a LTSV record mapping.
+     * @param bool $includeContext Whether to include context fields in a LTSV record.
+     * @param bool $includeExtra Whether to include extra fields in a LTSV record.
+     * @param array $labelReplacement Rule of replacement for LTSV labels.
+     * @param array $valueReplacement Rule of replacement for LTSV values.
+     */
+    public function __construct(
+        $dateFormat = null,
+        array $labeling = array('datetime' => 'time', 'level_name' => 'level', 'message' => 'message'),
+        $includeContext = true,
+        $includeExtra = true,
+        array $labelReplacement = array("\r" => '', "\n" => '', "\t" => '', ':' => ''),
+        array $valueReplacement = array("\r" => '\r', "\n" => '\n', "\t" => '\t')
+    )
+    {
+        parent::__construct($dateFormat);
+        $this->labeling = $labeling;
+        $this->includeContext = $includeContext;
+        $this->includeExtra = $includeExtra;
+        $this->labelReplacement = $labelReplacement;
+        $this->valueReplacement = $valueReplacement;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function format(array $record)
+    {
+        $builder = new LtsvLineBuilder($this->labelReplacement, $this->valueReplacement);
+
+        $ltsvRecord = array();
+        foreach ($this->labeling as $monologKey => $ltsvLabel) {
+            if (isset($record[$monologKey])) {
+                $ltsvRecord[$ltsvLabel] = $record[$monologKey];
+            }
+        }
+        $builder->addRecord($this->normalizeRecord($ltsvRecord));
+
+        if ($this->includeContext && isset($record['context'])) {
+            $builder->addRecord($this->normalizeRecord($record['context']));
+        }
+
+        if ($this->includeExtra && isset($record['extra'])) {
+            $builder->addRecord($this->normalizeRecord($record['extra']));
+        }
+
+        return $builder->build(true);
+    }
+
+    /**
+     * @param array $record
+     * @return array
+     */
+    protected function normalizeRecord(array $record)
+    {
+        $normalized = $this->normalize($record);
+        $converted = array();
+        foreach ($normalized as $key => $value) {
+            $converted[$key] = $this->convertToString($value);
+        }
+        return $converted;
+    }
+
+    /**
+     * @param mixed $data
+     * @return string
+     */
+    protected function convertToString($data)
+    {
+        if (null === $data || is_bool($data)) {
+            return var_export($data, true);
+        }
+
+        if (is_scalar($data)) {
+            return (string) $data;
+        }
+
+        return $this->toJson($data, true);
+    }
+}
---
+++ test/Monolog/Formatter/LtsvFormatterTest.php
@@ -0,0 +1,99 @@
+<?php
+namespace Hikaeme\Monolog\Formatter;
+
+use Monolog\Logger;
+
+class LtsvFormatterTest extends \PHPUnit_Framework_TestCase
+{
+    public function testFormat()
+    {
+        $dateTime = new \DateTime;
+        $record = $this->getRecord(
+            $dateTime,
+            "t\ne\ts\rt\\",
+            array("a:b\nc" => "d:e\rf"),
+            array("g:h\ti" => "j:k\nl", "m:n\\o" => "p:q\\r")
+        );
+
+        $formatter = new LtsvFormatter('Y-m-d');
+        $formatted = $formatter->format($record);
+        $expected = join("\t", array(
+                'time:' . $dateTime->format('Y-m-d'),
+                'level:WARNING',
+                'message:t\ne\ts\rt\\',
+                'abc:d:e\rf',
+                'ghi:j:k\nl',
+                'mn\\o:p:q\\r',
+            )) . PHP_EOL;
+        $this->assertSame($expected, $formatted);
+    }
+
+    public function testFormatWithDuplicatedLabel()
+    {
+        $dateTime = new \DateTime;
+        $record = $this->getRecord(
+            $dateTime,
+            "test",
+            array('k1' => 'v1', 'message' => 'message'),
+            array('k1' => 'v2')
+        );
+
+        $formatter = new LtsvFormatter('Y-m-d');
+        $formatted = $formatter->format($record);
+        $expected = join("\t", array(
+                'time:' . $dateTime->format('Y-m-d'),
+                'level:WARNING',
+                'message:test',
+                'k1:v1',
+                'message:message',
+                'k1:v2',
+            )) . PHP_EOL;
+        $this->assertSame($expected, $formatted);
+    }
+
+    public function testFormatWithSettings()
+    {
+        $dateTime = new \DateTime;
+        $record = $this->getRecord(
+            $dateTime,
+            "test\ntest",
+            array('k1' => 'v1'),
+            array('k2' => 'v2')
+        );
+
+        $formatter = new LtsvFormatter(
+            'H:i:s Y-m-d',
+            array('datetime' => 'date', 'message' => "mess\nage"),
+            false,
+            false,
+            array(),
+            array()
+        );
+        $formatted = $formatter->format($record);
+        $expected = join("\t", array(
+                'date:' . $dateTime->format('H:i:s Y-m-d'),
+                "mess\nage:test\ntest",
+            )) . PHP_EOL;
+        $this->assertSame($expected, $formatted);
+    }
+
+    /**
+     * @param \DateTime $dateTime
+     * @param string $message
+     * @param array $context
+     * @param array $extra
+     * @return array Record
+     */
+    private function getRecord(\DateTime $dateTime, $message = 'test', $context = array(), $extra = array())
+    {
+        return array(
+            'message' => $message,
+            'context' => $context,
+            'level' => Logger::WARNING,
+            'level_name' => Logger::getLevelName(Logger::WARNING),
+            'channel' => 'test',
+            'datetime' => $dateTime,
+            'extra' => $extra,
+        );
+    }
+}
